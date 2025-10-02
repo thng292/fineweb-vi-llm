6933acfa71855fabf980ce86c95a0cdf3e2f124f
thng292 <nguyenquangthong292@gmail.com>
2025-10-02 09:17:30 +0700

    [chore][training] No eval on start

--- scripts/launch_train.sh
+++ scripts/launch_train.sh
@@ -4,7 +4,7 @@ UV_ENV_FILE=.env CUDA_VISIBLE_DEVICES=0 uv run accelerate launch --multi_gpu -m 
     --checkpoint-dir outputs/training-251109 \
     --checkpoint-step 0.1 \
     --eval-step 0.1 \
-    --batch-size-per-device 1 \
+    --batch-size-per-device 2 \
     --gradient-accumulation 1 \
     --eval-batch-size-per-device 1 \
     --epochs 0.01 \
